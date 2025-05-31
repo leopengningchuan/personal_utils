46be4962714eb2d40edf324b49654cc8017f81a0
Leo Peng <leo.peng.ningchuan@gmail.com>
2025-05-31 11:24:48 -0700

    Update docx_manipulate.py
    - add the function merge_pdfs()

--- docx_manipulate.py
+++ docx_manipulate.py
@@ -1,7 +1,10 @@
 # import the packages
-from docx import Document
 import os
+import warnings
+from docx import Document
 from docx2pdf import convert
+from PyPDF2 import PdfMerger
+
 
 # define the function of replacing the template placeholder in tables with item dictionary information
 def populate_docx_table(item_dict, docx_template_path, new_docx_path):
@@ -123,3 +126,66 @@ def convert_docx_pdf(docx_path, keep = True):
         print(f'---------- {docx_path} converted to PDF successfully, original docx file removed.')
     else:
         print(f'---------- {docx_path} converted to PDF successfully, original docx file kept.')
+
+#---
+
+def merge_pdfs(pdf_list, output_path):
+    """
+    Merge multiple PDF files into a single PDF document.
+
+    Parameters:
+        pdf_list (list or str): A list of PDF file paths or a folder path containing PDF files.
+        output_path (str): The path where the merged PDF will be saved. Must end with '.pdf'.
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: If the input is not a list of PDF paths or a valid folder path,
+                   or if any file in the original PDF list is not a PDF,
+                   or if the output file path does not end with '.pdf',
+                   or if the final PDF list for combination does not contain any PDF file.
+        UserWarning: If the folder contains any non-PDF files.
+    """
+
+    # check the errors for file type
+    if not output_path.endswith('.pdf'):
+        raise TypeError("output_path should be a pdf file.")
+
+    if isinstance(pdf_list, list):
+        for file in pdf_list:
+            if not file.endswith('.pdf'):
+                raise TypeError(f"{file} is not a pdf file.")
+
+    elif os.path.isdir(pdf_list):
+        folder_path = pdf_list
+        pdf_list = os.listdir(pdf_list)
+        non_pdf_list = [file for file in pdf_list if not file.endswith('.pdf')]
+        pdf_list = [folder_path + '/' + file for file in pdf_list if file.endswith('.pdf')]
+
+        if len(non_pdf_list) == 1:
+            warnings.warn(f"{len(non_pdf_list)} file in the folder is not a pdf file.")
+        elif len(non_pdf_list) > 1:
+            warnings.warn(f"{len(non_pdf_list)} files in the folder are not pdf files.")
+
+    else:
+        raise TypeError(f"{pdf_list} should be a list of pdf files or a valid folder path.")
+
+    # check the length of the pdf_list
+    if len(pdf_list) == 0:
+        raise TypeError(f"There is no pdf file in the merged file list.")
+    else:
+        print('---------- merge the following pdf files:')
+        for file in pdf_list:
+            print(file)
+
+    # merge the pdf files
+    merger = PdfMerger()
+
+    for pdf in pdf_list:
+        merger.append(pdf)
+
+    # save the merged pdf file to the output path; print reminder
+    merger.write(output_path)
+    merger.close()
+    print(f'---------- {output_path} merged successfully.')
